30c5484ed7d1b3893cfe2e65a1a91f674f36d76f
Gaurav kumar <23146007@campus.dsvv.in>
2024-08-27 17:50:07 +0530

    Design Login page

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,52 +1,71 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatefulWidget{
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
-@override
-_LoginScreenState createState()=>_LoginScreenState();
-
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen>{
-
+class _LoginScreenState extends State<LoginScreen> {
 // form key
   final _formKey = GlobalKey<FormState>();
 
 //editing controller
 
-final TextEditingController emailController = new  TextEditingController();
-final TextEditingController passwordController = new  TextEditingController();
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController passwordController = new TextEditingController();
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     // email field
     final emailField = TextFormField(
       autofocus: false,
       controller: emailController,
       keyboardType: TextInputType.emailAddress,
 
-      onSaved: (value)
-      {
+// validator
+      onSaved: (value) {
         emailController.text = value!;
-
       },
       textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.mail),
+          contentPadding: EdgeInsets.fromLTRB(15, 15, 15, 15),
+          hintText: "Enter Your Email",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
     );
 
-       // Password field
+    // Password field
     final passwordField = TextFormField(
-      autofocus: false,
-      controller: passwordController,
+        autofocus: false,
+        controller: passwordController,
+        obscureText: true,
+        onSaved: (value) {
+          passwordController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+            prefixIcon: Icon(Icons.vpn_key),
+            contentPadding: EdgeInsets.fromLTRB(15, 15, 15, 15),
+            hintText: "Enter Your Password",
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(15)
+                )
+                )
+                );
 
-      onSaved: (value)
-      {
-        passwordController.text = value!;
-
-      },
-      textInputAction: TextInputAction.next,
+    final loginButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.redAccent,
+      child: MaterialButton(
+        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        minWidth: MediaQuery.of(context).size.width,
+        onPressed: (){},
+        child: Text("Login here"),
+        ),
     );
-
 
     return Scaffold(
       backgroundColor: Colors.white,
@@ -56,16 +75,11 @@ final TextEditingController passwordController = new  TextEditingController();
             color: Colors.white,
             child: Form(
               key: _formKey,
-              child:Column(
-                children: <Widget>[
-                  emailField,
-                  passwordField
-                ]
-                ) ,
-              ),
+              child: Column(children: [emailField, passwordField, loginButton]),
+            ),
           ),
         ),
-        ),
+      ),
     );
   }
 }
